61cd0406409aa6c9f3814262300b446199898fbb
haru-nico <106537636+haru-nico@users.noreply.github.com>
2022-05-30 21:20:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.all('/lavalinks', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(3000 || process.env.PORT, () => {
-    console.log('Server is running on port 3000');
-});
+function keepAlive(){
+    server.listen(3000 || process.env.PORT, ()=>{console.log("Server is Ready!")});
+}
 module.exports = keepAlive;
