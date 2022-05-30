3e240cfd5603f20e643d23cd2bf9c79f824e3922
haru-nico <106537636+haru-nico@users.noreply.github.com>
2022-05-30 21:14:05 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.all('/', (req, res)=>{
+    res.send('Your bot is alive!')
+})
+
+
+app.all('/lavalinks', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3000 || process.env.PORT, () => {
+    console.log('Server is running on port 3000');
+});
